4a4a7ab2485bf2c07ab8b1ed251269058db2b972
Michael Milewski <saramic@gmail.com>
2024-05-19 20:58:50 +1000

    Attempt to understand some of my nvim configs

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,9 @@
 local builtin = require('telescope.builtin')
+-- " " pf - find in "all" files in "current directory"
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+-- CTRL P - find in "git" files only
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- " " ps - grep in files
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
